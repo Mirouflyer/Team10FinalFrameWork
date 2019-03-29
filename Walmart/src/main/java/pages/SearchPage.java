3df8f70e87a0935fe8bc20696bd27538010efdc1
Samiraitouarab <samir_aitouarab@yahoo.fr>
2019-03-28 22:30:05 -0400

    Databae, menuPage, searchPage and SignIn Test

---
+++ Walmart/src/main/java/pages/SearchPage.java
@@ -0,0 +1,58 @@
+package pages;
+
+import base.CommonAPI;
+import datasource.DataBase;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import reporting.TestLogger;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+public class SearchPage extends CommonAPI {
+
+    DataBase dataBase = new DataBase();
+
+    @FindBy(how = How.CSS, using ="#global-search-input" )
+    public static WebElement searchInputWebElement;
+
+    @FindBy(how = How.CSS, using =".a1qUqR.a2GoU-.GlobalHeaderSearchbar-submit.a2btJm" )
+    public static WebElement submitButtonWebElement;
+
+    public WebElement getSearchInputWebElement(){
+        return searchInputWebElement;
+    }
+
+    public WebElement getSubmitButtonWebElement(){
+        return submitButtonWebElement;
+    }
+
+    public void searchFor(String value) {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName())+ " "+ value);
+        getSearchInputWebElement().sendKeys(value);
+    }
+
+    public void submitSearchButton(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        getSubmitButtonWebElement().click();
+
+    }
+
+    public void clearInput(){
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        getSearchInputWebElement().clear();
+    }
+
+    public void searchItemAndSubmitButton() throws Exception, IOException, SQLException, ClassNotFoundException {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        List<String> list = dataBase.getItemListFromDB();
+        for(int i = 0; i<list.size(); i++){
+            searchFor(list.get(i));
+            submitSearchButton();
+            clearInput();
+        }
+    }
+
+}
---
+++ Walmart/src/test/java/testSearch/TestSearchPage.java
@@ -0,0 +1,22 @@
+package testSearch;
+
+import base.CommonAPI;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+import pages.SearchPage;
+import reporting.ApplicationLog;
+import reporting.TestLogger;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class TestSearchPage extends CommonAPI {
+
+    @Test
+    public void searchPage() throws Exception, IOException, SQLException, ClassNotFoundException {
+        ApplicationLog.epicLogger();
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        SearchPage searchPage = PageFactory.initElements(driver,SearchPage.class);
+        searchPage.searchItemAndSubmitButton();
+    }
+}
